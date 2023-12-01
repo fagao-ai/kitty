386545320697fdb8fc5993ef6a54cbb54ed2a85e
hezhaozhao <hezhaozhaog@gmail.com>
2023-12-01 23:25:38 +0800

    add database sqlite for rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,25 @@
+mod database;
 mod hysteria;
+mod state;
+
 use hysteria::HyConfig;
 use hysteria_rs::start_from_json;
 
+use state::{AppState, ServiceAccess};
+use tauri::{AppHandle, Manager, State};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn greet(app_handle: AppHandle, name: &str) -> String {
+    // Should handle errors instead of unwrapping here
+    app_handle.db(|db| database::add_item(name, db)).unwrap();
 
+    let items = app_handle.db(|db| database::get_all(db)).unwrap();
+
+    let items_string = items.join(" | ");
+
+    format!("Your name log: {}", items_string)
+}
 #[tauri::command]
 fn start_hy(hy_config: HyConfig) {
     let serialized_hy_config = serde_json::to_string(&hy_config).unwrap();
@@ -46,6 +58,21 @@ mod tests {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState {
+            db: Default::default(),
+        })
+        .setup(|app| {
+            let handle = app.handle();
+            let aa = handle.path().app_local_data_dir();
+            println!("{:?}", aa);
+
+            let app_state: State<AppState> = handle.state();
+            let db =
+                database::initialize_database(&handle).expect("Database initialize should succeed");
+            *app_state.db.lock().unwrap() = Some(db);
+
+            Ok(())
+        })
         .plugin(tauri_plugin_window::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
