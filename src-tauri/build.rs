0b5049e431a3410bacc0044a063fb61f25a773b2
hezz <hezhaozhaog@gmail.com>
2023-12-18 23:18:04 +0800

    add download binarie to build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,40 +1,83 @@
-use git2::Repository;
-
 use anyhow::{Context, Result};
-use std::env;
+use build_target::{Arch, Os};
+use reqwest;
+use std::fs::{self, File};
+use std::os::unix::fs::PermissionsExt;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::{env, io};
 
-fn _build_hysteria() -> Result<()> {
-    let repo_url = "https://github.com/apernet/hysteria.git";
-    let repo_path = env::var("OUT_DIR").unwrap();
-    let _repo = match Repository::clone(repo_url, &repo_path) {
-        Ok(repo) => repo,
-        Err(e) => panic!("Failed to clone repository: {}", e),
+fn download_file(url: &str, file_name: &str) -> PathBuf {
+    let mut file =
+        reqwest::blocking::get(url).expect(format!("download {} failed!", file_name).as_str());
+
+    let project_dir = env::var("CARGO_MANIFEST_DIR").expect("Failed to get CARGO_MANIFEST_DIR");
+    let project_dir = Path::new(&project_dir);
+    let binaries_path = project_dir.join("binaries").join(file_name);
+    let mut out = File::create(&binaries_path).expect("failed to create file");
+    io::copy(&mut file, &mut out).expect("failed to copy content");
+    binaries_path
+}
+
+fn download_hysteria() {
+    let target = build_target::target().unwrap();
+    let source_name = match target.os {
+        Os::Windows => {
+            let os_name = "windows";
+            let arch = match target.arch {
+                Arch::X86_64 => "amd64",
+                Arch::X86 => "386",
+                Arch::AARCH64 => "arm64",
+                _ => panic!("Not support this arch."),
+            };
+            format!("hysteria-{}-{}.exe", os_name, arch)
+        }
+        Os::Linux => {
+            let os_name = "linux";
+            let arch = match target.arch {
+                Arch::X86_64 => "amd64",
+                Arch::X86 => "386",
+                Arch::ARM => "arm",
+                Arch::THUMBV6 => "armv5",
+                Arch::AARCH64 => "arm64",
+                Arch::S390X => "s390x",
+                Arch::MIPS => "mipsle",
+                Arch::RISCV => "riscv64",
+                _ => panic!("Not support this arch."),
+            };
+            format!("hysteria-{}-{}", os_name, arch)
+        }
+        Os::MacOs => {
+            let os_name = "linux";
+            let arch = match target.arch {
+                Arch::X86_64 => "amd64",
+                Arch::AARCH64 => "arm64",
+                _ => panic!("Not support this arch."),
+            };
+            format!("hysteria-{}-{}", os_name, arch)
+        }
+        _ => panic!("Not support this system."),
     };
-    env::set_current_dir(&repo_path).unwrap();
-    let _platform = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let mut command = Command::new("python");
-    command
-        .arg("hyperbole.py")
-        .arg("build")
-        .arg("-r")
-        .output()
-        .expect("Failed to execute complie hysteria faild.");
-    let command_str = format!("{:?}", command);
-    println!("command_str: {}", command_str);
-    let command_output = command
-        .spawn()
-        .context("fail spawning hysteria build")?
-        .wait_with_output()?;
-    if !command_output.status.success() {
-        let stdout = String::from_utf8_lossy(&command_output.stdout);
-        println!("Command output:\n{}", stdout);
-    } else {
-        let stderr = String::from_utf8_lossy(&command_output.stderr);
-        eprintln!("Command failed with error:\n{}", stderr);
-    }
+    let download_url = format!("https://download.hysteria.network/app/latest/{source_name}");
 
-    Ok(())
+    let target_name = format!("hysteria-{}", target.triple);
+    let binaries_path = download_file(download_url.as_str(), target_name.as_str());
+
+    let mut perms = fs::metadata(&binaries_path)
+        .expect("metadata failed.")
+        .permissions();
+    perms.set_mode(0o755);
+    let _ = fs::set_permissions(binaries_path, perms);
+}
+
+fn download_xray() {}
+
+fn download_binaries() {
+    let hysteria_feature = env::var("CARGO_FEATURE_HYSTERIA").is_ok();
+    let xray_feature = env::var("CARGO_FEATURE_XRAY").is_ok();
+    if !hysteria_feature && !xray_feature {
+        let _ = download_hysteria();
+    }
 }
 
 fn main() {
