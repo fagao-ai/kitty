011fe59b927759e50d70f6ee964a5a97a574e8a4
hezz <hezhaozhaoig@gmail.com>
2023-12-18 18:47:26 +0800

    add get_active_network_interface for macos

--- src-tauri/src/proxy/system_proxy.rs
+++ src-tauri/src/proxy/system_proxy.rs
@@ -48,11 +48,56 @@ pub fn set_system_proxy(host: &str, socks_port: u16, http_port: Option<u16>) -> 
 }
 
 #[cfg(target_os = "macos")]
+fn get_active_network_interface() -> Result<String> {
+    use anyhow::anyhow;
+    use std::net::Ipv4Addr;
+    use std::str::FromStr;
+    use std::{collections::HashMap, process::Command};
+    // networksetup -listallnetworkservices
+    let output = Command::new("networksetup")
+        .arg("-listallnetworkservices")
+        .output()?;
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let lines = stdout.lines();
+        let lines: Vec<&str> = lines.filter(|x| !x.contains("asterisk (*)")).collect();
+        for line in lines {
+            let info = Command::new("networksetup")
+                .arg("-getinfo")
+                .arg(line.trim())
+                .output()?;
+            if info.status.success() {
+                let mut info_map: HashMap<String, String> = HashMap::new();
+                let stdout = String::from_utf8_lossy(&info.stdout);
+                let lines = stdout.lines();
+                for line in lines {
+                    if line.contains(":") {
+                        let mut parts = line.trim().splitn(2, ':').map(str::trim);
+                        if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
+                            info_map.insert(key.to_string(), value.to_string());
+                        }
+                    }
+                }
+                if let Some(value) = info_map.get("IP address") {
+                    match Ipv4Addr::from_str(value) {
+                        Ok(res) => {
+                            return Ok(res.to_owned().to_string());
+                        }
+                        Err(_) => continue,
+                    }
+                }
+            }
+        }
+    }
+    Err(anyhow!("Not found aviliable network interface."))
+}
+
+#[cfg(target_os = "macos")]
 pub fn set_system_proxy(host: &str, socks_port: u16, http_port: Option<u16>) -> Result<&str> {
     use anyhow::Ok;
 
     let service = "Wi-Fi";
-    let mut socks_sysproxy = Sysproxy {
+    let socks_sysproxy = Sysproxy {
         enable: true,
         host: host.into(),
         port: socks_port,
@@ -61,7 +106,7 @@ pub fn set_system_proxy(host: &str, socks_port: u16, http_port: Option<u16>) -> 
     let _ = socks_sysproxy.set_socks("0x10");
     match http_port {
         Some(http_port) => {
-            let mut socks_sysproxy = Sysproxy {
+            let socks_sysproxy = Sysproxy {
                 enable: true,
                 host: host.into(),
                 port: http_port,
