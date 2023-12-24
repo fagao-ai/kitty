f106b1de23a09121b9855cd5b59be6069ffa8cda
zhangluguang <z397503810@gmail.com>
2023-12-24 22:19:22 +0800

    feat: fix error message toast

--- src/utils/invoke.ts
+++ src/utils/invoke.ts
@@ -1,14 +1,12 @@
 import { invoke as tauriInvoke } from '@tauri-apps/api/primitives'
 import { camelizeKeys } from 'humps'
+import { useMessage } from 'naive-ui'
 import type { InvokeArgs, InvokeOptions } from '@tauri-apps/api/types/primitives'
-import {
-  isPermissionGranted,
-  requestPermission,
-  sendNotification,
-} from '@tauri-apps/plugin-notification'
 import type { KittyResponse } from '@/types'
 
 export async function invoke<T>(cmd: string, args?: InvokeArgs, options?: InvokeOptions): Promise<KittyResponse<T>> {
+  const message = useMessage()
+
   try {
     if (import.meta.env.KITTY_ENV !== 'web') {
       const resp = await tauriInvoke<KittyResponse<T>>(cmd, args, options)
@@ -26,20 +24,20 @@ export async function invoke<T>(cmd: string, args?: InvokeArgs, options?: Invoke
     return camelizeKeys(resp.json()) as unknown as KittyResponse<T>
   }
   catch (e) {
-    // message.error(`${e}`, { duration: 3000 })
+    message.error(`subeeeee`, { duration: 3000 })
     console.error('kitty error', e)
 
     throw e
   }
   finally {
-    let permissionGranted = await isPermissionGranted()
-    if (!permissionGranted) {
-      const permission = await requestPermission()
-      permissionGranted = permission === 'granted'
-    }
-    if (permissionGranted) {
-      sendNotification('Tauri is awesome!')
-      sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' })
-    }
+    // let permissionGranted = await isPermissionGranted()
+    // if (!permissionGranted) {
+    //   const permission = await requestPermission()
+    //   permissionGranted = permission === 'granted'
+    // }
+    // if (permissionGranted) {
+    //   sendNotification('Tauri is awesome!')
+    //   sendNotification({ title: 'TAURI', body: 'Tauri is awesome!' })
+    // }
   }
 }
