5ac4d5934a4ea81d74795cc9ce3281d664140109
hezz <hezhaozhaog@gmail.com>
2024-01-16 22:34:54 +0800

    add xray struct example

--- src-tauri/migration/src/m20231223_035153_create_xray.rs
+++ src-tauri/migration/src/m20231223_035153_create_xray.rs
@@ -37,3 +37,58 @@ enum Xray {
     Id,
     Name,
 }
+
+
+// struct Mux {
+//     pub enabled: bool,
+//     pub concurrency: i64,
+// }
+//
+// struct Headers {
+//     pub host: String,
+// }
+//
+// struct WsSettings {
+//     pub path: String,
+//     pub headers: Headers,
+// }
+//
+// struct TlsSettings {
+//     pub allow_insecure: bool,
+//     pub server_name: String,
+//     pub fingerprint: String,
+// }
+//
+// struct StreamSettings {
+//     pub network: String,
+//     pub security: String,
+//     pub tls_settings: TlsSettings,
+//     pub ws_settings: WsSettings,
+// }
+//
+// struct Struct1 {
+//     pub id: String,
+//     pub alter_id: i64,
+//     pub email: String,
+//     pub security: String,
+//     pub encryption: String,
+//     pub flow: String,
+// }
+//
+// struct Struct {
+//     pub address: String,
+//     pub port: i64,
+//     pub users: Vec<Struct1>,
+// }
+//
+// struct Settings {
+//     pub vnext: Vec<Struct>,
+// }
+//
+// struct Root {
+//     pub tag: String,
+//     pub protocol: String,
+//     pub settings: Settings,
+//     pub stream_settings: StreamSettings,
+//     pub mux: Mux,
+// }
