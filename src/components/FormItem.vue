b40aac830a036a569c5031115806581fc4c991fa
sube <z397503810@163.com>
2024-01-23 00:55:13 +0800

    feat: form item components

---
+++ src/components/FormItem.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { NFormItem, NInput, NSelect } from 'naive-ui'
+import { useVModel } from '@vueuse/core'
+import { FormItem } from '@/composables/useFormItem'
+
+interface Props {
+  form: FormItem | null
+}
+const props = defineProps<Props>()
+
+const formState = useVModel(props, 'form')
+
+function getNext(): FormItem | null {
+  let current = formState.value
+  if (!current)
+    return null
+
+  const acients = []
+  acients.unshift(current)
+
+  while (current) {
+    acients.unshift(current)
+    current = current.parent
+  }
+
+  return formState.value!.next(formState.value!, acients)
+}
+</script>
+
+<template>
+  <template v-if="formState">
+    <n-form-item :label="formState.payload.label">
+      <template v-if="formState.payload.type === 'input'">
+        <n-input v-model:value="formState.payload.value" />
+      </template>
+      <template v-else-if="formState.payload.type === 'textarea'">
+        <n-input v-model:value="formState.payload.value" type="textarea" />
+      </template>
+      <template v-else-if="formState.payload.type === 'select'">
+        <n-select v-model:value="formState.payload.value" :options="formState.payload.options" />
+      </template>
+    </n-form-item>
+    <form-item :form-state="getNext()" />
+  </template>
+</template>
---
+++ src/composables/useFormItem.ts
@@ -0,0 +1,43 @@
+import { isReactive, reactive } from 'vue'
+
+export type FormItemType = 'input' | 'select' | 'textarea' | 'checkbox' | 'radio'
+
+export interface FormItem<T = any> {
+  type: FormItemType
+  payload: T
+  next: (current: FormItem<T>, acients: FormItem<T>[]) => FormItem<T> | null
+  parent: FormItem<T> | null
+}
+
+export function useFormItem<T>() {
+  function createFormItem(formItemType: FormItem<T>['type'], payload: FormItem<T>['payload'], next?: FormItem<T>['next'], parent?: FormItem<T>['parent']): FormItem<T> {
+    if (!next)
+      next = () => null
+
+    if (!parent)
+      parent = null
+
+    function nextFuncion(current: FormItem<T>, acients: FormItem<T>[]) {
+      let nextItem = next!(current, acients)
+      if (!nextItem)
+        return null
+
+      nextItem.parent = current
+      if (!isReactive(nextItem))
+        nextItem = reactive(nextItem) as FormItem<T>
+
+      return nextItem
+    }
+
+    const formItem: FormItem<T> = reactive({
+      type: formItemType,
+      payload,
+      next: nextFuncion,
+      parent,
+    }) as FormItem<T>
+
+    return formItem
+  }
+
+  return { createFormItem }
+}
