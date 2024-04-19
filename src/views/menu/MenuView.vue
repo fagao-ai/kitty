7713e6f05ecb23ec71bfa38782a3ecc850461efe
hezz <hezhaozhaoig@gmail.com>
2024-04-19 13:41:52 +0800

    upgrade tauri version

--- src/views/menu/MenuView.vue
+++ src/views/menu/MenuView.vue
@@ -97,7 +97,7 @@ window.$message = useMessage()
         {{ t('menubar.version') }}
       </div>
       <div class="text-primay text-xs">
-        0.0.1.beta
+        0.0.2.beta
       </div>
     </div>
   </div>
