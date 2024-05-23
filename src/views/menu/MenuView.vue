49b513a1c6119b92da42829e7ac1fc6936413c93
hezz <hezhaozhaoig@gmail.com>
2024-05-23 13:35:10 +0800

    change hysteria to v2.4.4

--- src/views/menu/MenuView.vue
+++ src/views/menu/MenuView.vue
@@ -97,7 +97,7 @@ window.$message = useMessage()
         {{ t('menubar.version') }}
       </div>
       <div class="text-primay text-xs">
-        0.0.2.1.beta
+        0.0.3.beta
       </div>
     </div>
   </div>
