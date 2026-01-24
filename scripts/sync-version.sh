77c5fbfa80d11ae9ec939fc8379e2a8d6c7f699a
kitty <kitty.eu.org@gmail.com>
2026-01-24 13:01:24 +0800

    update sync version script

--- scripts/sync-version.sh
+++ scripts/sync-version.sh
@@ -4,3 +4,7 @@
 VERSION=$(node -p "require('./package.json').version")
 sed -i "s/^version = .*/version = \"$VERSION\"/" src-tauri/Cargo.toml
 echo "Updated Cargo.toml version to $VERSION"
+
+# Create git tag with v prefix
+git tag -f "v$VERSION"
+echo "Created tag v$VERSION"
